a0984ce9e199ca4cc7821f5d7958b4893d2e138d
Clayton Stanley <cstanley25@bloomberg.net>
2017-07-25 13:51:29 -0400

    Jittering the times

--- build.R
+++ build.R
@@ -4,9 +4,10 @@ library(knitr)
 library(markdown)
 
 sessionTbl = data.table(rID=1:100000)
-sessionTbl
+sessionTbl[, rID := 1:.N]
 sessionTbl[, iDateTime := as.POSIXct('2017-01-01T00:00:01', format="%Y-%m-%dT%H:%M:%OS", tz='GMT')]
-sessionTbl[, iDateTime := iDateTime + rID]
+sessionTbl[, iDateTime := iDateTime + rID ^ (1.1)]
+sessionTbl
 dtTbl = sessionTbl[, IDateTime(iDateTime)]
 sessionTbl[, c('idate', 'itime') := dtTbl]
 sessionTbl[, sID := ceiling(rID / 3)]
@@ -17,6 +18,9 @@ sessionTbl[, rID := NULL]
 sessionTbl[, label := sample(c('a', 'b', 'c', 'd', 'e', 'f', 'g'), NS, replace=T, prob=1:7), sID]
 sessionTbl
 
+
+library(shiny.alluvial)
+runAppSK(sessionTbl)
 devtools::use_package("inline")
 devtools::use_package("magrittr")
 
